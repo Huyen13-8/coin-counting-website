5f7aedf8d716ed938e2da8ed1d9d78cace50d136
Khanh Huyen Nguyen <khanhhuyen131hp@gmail.com>
2026-06-20 12:40:01 -0400

    Fixed allow origins in back end main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-    "https://coin-counter.vercel.app",],
+    "https://coin-counting-website.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
